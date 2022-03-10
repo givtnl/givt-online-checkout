201782e683a8a62ec8e4086ed93ea278a39a67ca
Michiel Verkest <michiel@Michiels-MacBook-Pro.local>
2022-03-10 11:41:53 +0100

    add second page and arrow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'givt-online-checkout';
+  page = 1;
+
+  incrementPage() {
+    this.page++
+  }
+
+  decrementPage() {
+    this.page--
+  }
 }
